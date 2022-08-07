796a645bf7fc62e4c4acae6e916a5568e3b9f218
PavelMetlin <metlin-1@mail.ru>
2022-08-07 13:44:59 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = Convert.ToInt16(Console.ReadLine());
+int numberB = Convert.ToInt16(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine("MAX");
+}
+else
+
+{
+    Console.WriteLine("MIN");
+}
+
