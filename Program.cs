388f855e5ab17d1ca7cd8afaaa94baea1f65dd04
PavelMetlin <metlin-1@mail.ru>
2022-08-07 15:38:37 +0300

    initial

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+    
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);
